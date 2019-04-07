418334a1e65a9007fe7dfbb2502a21359c1501f6
matthew9602 <matthew9602@qq.com>
2019-04-06 21:53:08 -0400

    complete 002

--- 002/002.py
+++ 002/002.py
@@ -4,7 +4,17 @@ class ListNode:
         self.val = x
         self.next = None
 
+
 class Solution:
+
+    def Node2List(self, head):
+        p = head
+        list = [p.val]
+        while p.next != None:
+            p = p.next
+            list.append(p.val)
+        return list
+
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
         p1 = l1
         p2 = l2
@@ -16,24 +26,37 @@ class Solution:
         if p2.next == None and p1.next != None:
             p2.next = ListNode(0)
 
+        # num2Forms.print_ListNode(p1)
+        # num2Forms.print_ListNode(p2)
+        # num2Forms.print_ListNode(head)
+        # print(tem)
+
         while p1.next != None and p2.next != None:
             p1 = p1.next
             p2 = p2.next
             p.next = ListNode((p1.val + p2.val + tem) % 10)
             p = p.next
-            tem = (p1.val + p2.val) // 10
+            tem = (p1.val + p2.val + tem) // 10
+            # num2Forms.print_ListNode(p1)
+            # num2Forms.print_ListNode(p2)
+            # num2Forms.print_ListNode(head)
+            # print(tem)
             if p1.next == None and p2.next != None:
                 p1.next = ListNode(0)
             if p2.next == None and p1.next != None:
                 p2.next = ListNode(0)
 
+        # print(tem)
         if tem != 0:
             p.next = ListNode(tem)
 
-        return head
+        result = self.Node2List(head)
+        return result
 
-class num2ListNode:
-    def num2ListNode(self, num):
+
+class num2Forms:
+    @classmethod
+    def num2ListNode(cls, num):
         head = ListNode(num % 10)
         p = head
         num = num // 10
@@ -44,16 +67,35 @@ class num2ListNode:
             p = node
         return head
 
-    def print_ListNode(self, head):
+    @classmethod
+    def print_ListNode(cls, head):
         p = head
         print(p.val, end="")
         while p.next != None:
             p = p.next
             print(" -> ", p.val, end="")
+        print("\n")
+
+    @classmethod
+    def num2List(cls, num):
+        if num == 0:
+            return [0]
+        list = []
+        while num != 0:
+            list.append(num % 10)
+            num = num // 10
+        return list
 
 
 if __name__ == '__main__':
-    number1 = num2ListNode().num2ListNode(243)
-    number2 = num2ListNode().num2ListNode(564)
-    result = Solution().addTwoNumbers(number1, number2)
-    num2ListNode().print_ListNode(result)
+    for i in range(0, 10000):
+        for j in range(0, 10000):
+            number1 = num2Forms().num2ListNode(i)
+            number2 = num2Forms().num2ListNode(j)
+            result = Solution().addTwoNumbers(number1, number2)
+            if i % 50 == 0 and j == 0:
+                print(i, " ", j, " ", i+j, " ", result)
+            if result != num2Forms.num2List(i+j) :
+                print("%d + %d, expect: %d" % (i, j, i+j))
+                print("get:", result)
+                exit()
