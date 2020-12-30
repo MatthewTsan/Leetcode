770486437ebc326c30d7eec9ccab91074dd4eec4
MatthewTsan <matthew9602@qq.com>
2020-12-30 15:01:00 -0500

    complete q739

---
+++ java/src/main/java/Leetcode/q739/Solution.java
@@ -0,0 +1,17 @@
+package main.java.Leetcode.q739;
+import java.util.*;
+
+class Solution {
+    public int[] dailyTemperatures(int[] T) {
+        Stack<Integer> stack = new Stack<>();
+        int[] ans = new int[T.length];
+        for(int i=T.length-1; i>=0; i--){
+            while(!stack.isEmpty() && T[i] >= T[stack.peek()]){
+                stack.pop();
+            }
+            ans[i] = stack.isEmpty()? 0 : stack.peek() - i;
+            stack.push(i);
+        }
+        return ans;
+    }
+}
