3d97611316658e2548ee36635b53a8ea1ec7b3f9
MatthewTsan <matthew9602@qq.com>
2021-01-03 12:20:58 -0500

    complete q084

--- java/src/main/java/Leetcode/q084/Solution.java
+++ java/src/main/java/Leetcode/q084/Solution.java
@@ -1,5 +1,5 @@
 package main.java.Leetcode.q084;
-import java.util.*:
+import java.util.*;
 
 class Solution {
     public int largestRectangleArea(int[] heights) {
@@ -20,24 +20,5 @@ class Solution {
             maxArea = Math.max(maxArea, (heights.length - top) * heights[top]);
         }
         return maxArea;
-    }
-}
-
-public class Solution {
-    public int largestRectangleArea(int[] heights) {
-        Stack < Integer > stack = new Stack < > ();
-        stack.push(-1);
-        int maxarea = 0;
-        for (int i = 0; i < heights.length; ++i) {
-            System.out.println(stack.toString());
-            while (stack.peek() != -1 && heights[stack.peek()] >= heights[i])
-                maxarea = Math.max(maxarea, heights[stack.pop()] * (i - stack.peek() - 1));
-            stack.push(i);
-        }
-        while (stack.peek() != -1){
-            System.out.println(stack.toString());
-            maxarea = Math.max(maxarea, heights[stack.pop()] * (heights.length - stack.peek() -1));
-        }
-        return maxarea;
     }
 }
